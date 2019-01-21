4c267c6351760e9d4b01368f04e3539fe3c61871
Phodal HUANG <h@phodal.com>
2019-01-21 11:39:21 +0800

    feat: add default parsed results

--- src/main.js
+++ src/main.js
@@ -24,6 +24,19 @@ var originDimensions = [
 var dimensions = {};
 
 function initForm() {
+  window.results = [];
+  window.lastResult = [];
+  var savedItems = window.localStorage.getItem('tla');
+  var lastrResults = window.localStorage.getItem('tla.last.results');
+  if (savedItems) {
+    var parsedItems = JSON.parse(savedItems);
+    drawChart(parsedItems);
+    window.results = parsedItems;
+  }
+  if (lastrResults) {
+    window.lastResults = JSON.parse(lastrResults);
+  }
+
   var sections = document.getElementById('sections');
   var sectionsHtml = "";
   for (var i = 0; i < originDimensions.length; i++) {
@@ -35,14 +48,17 @@ function initForm() {
       id: id,
       axis: dimension
     };
+    var value = 3;
+    if (window.lastResults) {
+      value = window.lastResults[id].value;
+    }
     sectionsHtml = sectionsHtml + '  <section>\n' +
       '    <label for="' + id + '">' + dimension + '</label>\n' +
-      '    <input id="' + id + '" type="range" min="0" max="5" step="1" data-orientation="horizontal">\n' +
+      '    <input id="' + id + '" type="range" min="0" max="5" step="1" value="' + value + '" data-orientation="horizontal">\n' +
       '  </section>\n'
   }
 
   sections.innerHTML = sectionsHtml;
-
 }
 
 initForm();
@@ -53,12 +69,25 @@ function handleClick() {
   var arr = Object.keys(dimensions);
   for (var i = 0; i < arr.length; i++) {
     var currentDimension = dimensions[arr[i]];
-    console.log(currentDimension, arr[i]);
     var value = document.getElementById(arr[i]).value;
+    let parsedValue = parseInt(value);
     results.push({
       axis: currentDimension.axis,
-      value: parseInt(value)
-    })
+      value: parsedValue
+    });
+
+    dimensions[arr[i]].value = parsedValue;
   }
-  drawChart([results]);
+
+  if (window.results.length <= 1) {
+    window.results.push(results);
+  } else {
+    window.results.shift();
+    window.results.push(results);
+  }
+
+  localStorage.setItem('tla', JSON.stringify(window.results));
+  localStorage.setItem('tla.last.results', JSON.stringify(dimensions));
+
+  drawChart(window.results);
 }
